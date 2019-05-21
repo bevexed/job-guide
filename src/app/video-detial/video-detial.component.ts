00f8352b8e435f319b89841aa8ad3cdd77ed3153
bevexed <547551650@qq.com>
2019-05-21 14:26:39 +0800

    首页 二次修改

--- src/app/video-detial/video-detial.component.ts
+++ src/app/video-detial/video-detial.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { HttpService } from '../http.service';
-import { NzModalService, NzMessageService } from 'ng-zorro-antd';
-import { fromEvent } from 'rxjs';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {HttpService} from '../http.service';
+import {NzModalService, NzMessageService} from 'ng-zorro-antd';
+import {fromEvent} from 'rxjs';
 
 
 @Component({
@@ -12,7 +12,7 @@ import { fromEvent } from 'rxjs';
 })
 export class VideoDetialComponent implements OnInit {
   public courseInfo: any;
-  public isplay: boolean = false;
+  public isplay = false;
   public courseId: number;
 
   constructor(
@@ -21,39 +21,44 @@ export class VideoDetialComponent implements OnInit {
     private modalService: NzModalService,
     private message: NzMessageService,
     private router: Router
-  ) { }
+  ) {
+  }
 
   ngOnInit() {
     this.route.params.subscribe((res: any) => {
       this.courseId = +res.id;
       this.getCourseDetail(this.courseId);
-    })
+    });
     if (!this.httpService.devType) {
       fromEvent(document.body, 'click').subscribe(() => {
         if (this.isplay) {
           this.isplay = false;
         }
-      })
+      });
     }
   }
 
   public async getCourseDetail(id: number) {
-    let res = await this.httpService.getCourseDetail(id);
+    const res = await this.httpService.getCourseDetail(id);
     if (res.code === 200) {
       this.courseInfo = res.data;
-      if (!this.courseInfo.freeFlag) { /** 如果vipFlag为false但却获取到了VIP视频的url,那么证明本地数据未更新，前端主动更新 */
+      if (!this.courseInfo.freeFlag) {
+        /** 如果vipFlag为false但却获取到了VIP视频的url,那么证明本地数据未更新，前端主动更新 */
         this.httpService.tokenValidate();
       }
     } else {
       this.message.create('error', res.message);
       setTimeout(() => {
         this.router.navigate(['../index']);
-      }, 2000)
+      }, 2000);
     }
   }
 
-  public play(event?: any) {
-    event&&event.stopPropagation();
+  public play(event?: MouseEvent) {
+    if (event) {
+      event.stopPropagation();
+    }
+
     if (this.courseInfo.freeFlag) {
       this.isplay = true;
       this.httpService.coursePlay(this.courseId);
