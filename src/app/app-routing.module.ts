4355547d8369e53bfc5ab2d28d087dfab1fcbdf0
UDO <udo@appledeMacBook-Air.local>
2019-08-03 22:20:43 +0800

    xsw

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -26,7 +26,7 @@ const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    redirectTo: 'index'
+    redirectTo: 'profession'
   },
   {
     path: 'index',
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -70,7 +70,7 @@ export class LoginComponent implements OnInit {
         this.httpService.user = res.data.user;
         this.httpService.init();
         this.message.create('success', '登录成功');
-        this.router.navigate(['../index']);
+        this.router.navigate(['../profession']);
       } else {
         this.message.create('error', res.message);
       }
